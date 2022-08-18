6b751d1b08c957e12834db7ad44c0214801b4151
maq7 <maxpet16@mail.ru>
2022-08-18 23:19:41 +0500

    [weather-cli]: have been added conditional for empty token

--- weather-cli/weather.js
+++ weather-cli/weather.js
@@ -4,6 +4,9 @@ import { printHelp, printError, printSuccess } from "./service/log.service.js";
 import { saveKeyValue } from "./service/storage.service.js";
 
 const saveKey = async (token) => {
+    if(!token.length){
+        return printError("Token not transferred");
+    }
     try {
         await saveKeyValue("token", token);
         printSuccess("Token saved");
