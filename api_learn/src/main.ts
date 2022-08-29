2b3a1d1108d6b468504522c4ed5edd16648ee42b
maq7 <maxpet16@mail.ru>
2022-08-29 19:49:25 +0500

    [api_learn]: have been fixed app

--- api_learn/src/main.ts
+++ api_learn/src/main.ts
@@ -1,8 +1,10 @@
 import { App } from "./app";
+import { userRouter } from "./users/user";
 
 async function bootstrap() {
     const app = new App();
     await app.init();
+    app.useRoutes(userRouter)
 }
 
 bootstrap();
