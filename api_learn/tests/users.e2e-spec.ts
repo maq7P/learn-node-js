2cff40907507d3cf450ee29ab1c05e9307f62937
maq7 <maxpet16@mail.ru>
2022-10-02 23:22:24 +0500

    [api_learn]: have been cover a few endpoints

--- api_learn/tests/users.e2e-spec.ts
+++ api_learn/tests/users.e2e-spec.ts
@@ -12,6 +12,11 @@ const EXISTED_USER_IN_DB = {
 	name: "Коля",
 };
 
+const USER_WITH_WRONG_PASSWORD = {
+	...EXISTED_USER_IN_DB,
+	password: "WRONG PASSWORD",
+};
+
 beforeAll(async () => {
 	const { app } = await boot;
 	application = app;
@@ -23,6 +28,38 @@ describe("users e2e", () => {
 		const res = await request(application.app).post("/users/register").send(EXISTED_USER_IN_DB);
 
 		expect(res.statusCode).toBe(422);
+	});
+
+	it("Login - succes", async () => {
+		const res = await request(application.app).post("/users/register").send(EXISTED_USER_IN_DB);
+
+		expect(res.body.jwt).not.toBeUndefined;
+	});
+
+	it("Login - error", async () => {
+		const res = await request(application.app)
+			.post("/users/register")
+			.send(USER_WITH_WRONG_PASSWORD);
+
+		expect(res.statusCode).toBe(401);
+	});
+
+	it("Info - success", async () => {
+		const login = await request(application.app).post("/users/register").send(EXISTED_USER_IN_DB);
+
+		const res = await request(application.app)
+			.post("/users/info")
+			.set("Authorization", `Bearer: ${login.body.jwt}`);
+
+		expect(res.body.email).toBe(EXISTED_USER_IN_DB.email);
+	});
+
+	it("Info - error", async () => {
+		const res = await request(application.app)
+			.post("/users/info")
+			.set("Authorization", `Bearer: no`);
+
+		expect(res.statusCode).toBe(401);
 	});
 });
 
